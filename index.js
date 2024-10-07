659295b934df421726df4a59e60c0d327e1d9fa8
congxdanh <danh.hsb13@gmail.com>
2024-10-07 09:16:12 +0700

    next commit

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const express = require("express");
-const app = express();
 const quizzesRouter = require("./src/routes/quizzRouter");
 const questionsRouter = require("./src/routes/questionRouter");
 const connectDB = require("./src/config/connectDB");
+const app = express();
+require("dotenv").config();
+
+const cors = require("cors");
+
+app.use(
+  cors({
+    // origin: "http://localhost:3001", // Cho phép từ domain cụ thể của bạn
+    // credentials: true, // Cho phép gửi thông tin xác thực (nếu cần)
+  })
+);
+app.options("*", cors());
 
 app.use(express.json());
 connectDB();
@@ -11,7 +22,7 @@ app.use("/quizzes", quizzesRouter);
 app.use("/questions", questionsRouter);
 
 app.use("/", (req, res) => {
-  res.send("home");
+  res.send("Welcome to Question Bank Management");
 });
 app.listen(3001, () => {
   console.log("Server started on port 3001");
